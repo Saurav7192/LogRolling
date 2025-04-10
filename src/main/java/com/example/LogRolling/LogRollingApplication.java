7b946f340d6b5c024dab4bd03b1db859f61781c0
Saurav Kumar <sauravkumar71920@gmail.com>
2025-04-10 10:19:00 +0530

    Log Rolling Appender Project started

---
+++ src/main/java/com/example/LogRolling/LogRollingApplication.java
@@ -0,0 +1,13 @@
+package com.example.LogRolling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogRollingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogRollingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LogRolling/LogRollingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LogRolling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogRollingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
